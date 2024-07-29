23bc8d7f40e550cf5049a94d8a10ce4a49c835b7
Tianyue Yang <tianyue.yang22@imperial.ac.uk>
2024-07-29 15:53:49 +0800

    updated doc

--- src/pleiades/blocks/simple_self_attention.py
+++ src/pleiades/blocks/simple_self_attention.py
@@ -5,9 +5,14 @@ import jax
 
 class SelfAttention(nn.Module):
     """
-    This is a light wrapper around `nn.MultiheadAttention` with a GroupNorm and an output projection.
+    This is a light wrapper around `flax.linen.MultiheadAttention` with a GroupNorm and an output projection.
 
     :cvar output_channels: number of projected output channels.
+    :cvar group: number of groups used for GroupNorm.
+    :cvar attention_heads: number of attention heads.
+    :cvar use_qkv_bias: whether to use bias in the QKV matrix.
+    :cvar use_dropout: whether to use dropout in the attention layer.
+    :cvar dropout_rate: dropout rate for the attention dropout, only used if use_dropout=True.
 
     """
     output_channels: int
@@ -18,7 +23,7 @@ class SelfAttention(nn.Module):
     dropout_rate: float = 0.1
 
     @nn.compact
-    def __call__(self, x, train: bool):
+    def __call__(self, x: jnp.ndarray, train: bool) -> jnp.ndarray:
         x = nn.GroupNorm(num_groups=self.group if x.shape[-1] % self.group == 0 else x.shape[-1],
                          group_size=None)(x)
         x = nn.MultiHeadDotProductAttention(
